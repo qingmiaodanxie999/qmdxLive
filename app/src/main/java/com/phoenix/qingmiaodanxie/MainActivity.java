f8497586cf666919d2fc48ba8d5513c5c9f8824b
wd <397400245@qq.com>
2017-03-08 09:29:46 +0800

    test11

--- app/src/main/java/com/phoenix/qingmiaodanxie/MainActivity.java
+++ app/src/main/java/com/phoenix/qingmiaodanxie/MainActivity.java
@@ -2,12 +2,19 @@ package com.phoenix.qingmiaodanxie;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.tv);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
